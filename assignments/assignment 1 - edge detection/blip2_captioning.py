d7938d518bfaa2dcbdd251a9c3b96295c0944917
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2023-09-23 20:02:57 +0530

    add caption list

---
+++ assignments/assignment 1 - edge detection/blip2_captioning.py
@@ -0,0 +1,24 @@
+import torch
+from PIL import Image
+from lavis.models import load_model_and_preprocess
+import os
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model, vis_processors, _ = load_model_and_preprocess(name='blip_caption', model_type='base_coco', is_eval=True, device=device)
+
+def caption_blip(model, vis_processors, image):
+    image = vis_processors['eval'](image).unsqueeze(0).to(device)
+    caption = model.generate({"image": image})
+    return caption
+
+caption_list = []
+
+source_folder = 'natural images2'
+for image_path in os.listdir(source_folder):
+    image_path = os.path.join(source_folder, image_path)
+    image = Image.open(image_path)
+    caption = caption_blip(model, vis_processors, image)
+    caption_list.append(caption[0])
+
+with open('caption_list2.txt', 'w') as f:
+    f.writelines([caption + '\n' for caption in caption_list])
